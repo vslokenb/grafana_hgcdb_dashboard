69f4efdb7208b140518843b79e2509c08c50b809
vslokenb <108902248+vslokenb@users.noreply.github.com>
2026-06-08 11:22:30 -0500

    Update main.py
    more for the static dashboard tools.

--- main.py
+++ main.py
@@ -47,9 +47,10 @@ def main():
         print(" >>>> preSteps skipped.\n")
 
     # Everything Need To Generate
-    subprocess.run(["python", "create/create_folders.py"], check=True)
+    subprocess.run([sys.executable, "create/create_folders.py"], check=True)
     sleep(0.5)    # wait for folders to be added
-    subprocess.run(["python", "create/create_dashboards.py"], check=True)
+    subprocess.run([sys.executable, "create/create_dashboards.py"], check=True)
+    subprocess.run([sys.executable, "create/create_static_dashboards.py"], check=True)
     # subprocess.run(["python", "create/create_alerts.py"], check=True)
 
     # Add run times
